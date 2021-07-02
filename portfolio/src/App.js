da34134ace0972bc1aa990f24f7eeaa828d19bc8
Ian Applekamp <ieapplekamp@gmail.com>
2021-07-02 10:48:36 -0400

    initial commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hi Gay
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
